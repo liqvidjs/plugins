f1cb5013b82a531f64b24657226360f1a07e7565
Yuri Sulyma <yuri@liqvidjs.org>
2022-05-05 15:07:06 -0400

    cursor: add Promise support, fix useEffect subscription

--- packages/cursor/src/react.tsx
+++ packages/cursor/src/react.tsx
@@ -1,11 +1,15 @@
 import {usePlayback} from "@lqv/playback/react";
 import {useEffect, useRef} from "react";
 import {cursorReplay} from ".";
+type CursorData = Parameters<typeof cursorReplay>[0]["data"];
 
 /**
  * Move an image along a recorder cursor path. React version of {@link cursorReplay}.
  */
-export function Cursor(props: Omit<Parameters<typeof cursorReplay>[0], "playback" | "target"> & {
+export function Cursor(props: Omit<Parameters<typeof cursorReplay>[0], "data" | "playback" | "target"> & {
+  /** Cursor data to replay. */
+  data: CursorData | Promise<CursorData>;
+
   /** Src of cursor image. */
   src: string;
 }) {
@@ -14,11 +18,27 @@ export function Cursor(props: Omit<Parameters<typeof cursorReplay>[0], "playback
 
   // subscribe
   useEffect(() => {
-    return cursorReplay({
-      playback,
-      target: ref.current,
-      ...props
-    });
+    if (props.data instanceof Promise) {
+      let unsub: () => void;
+      props.data.then(data => {
+        unsub = cursorReplay({
+          playback,
+          target: ref.current,
+          ...props,
+          data
+        });
+      });
+      return () => {
+        unsub?.();
+      };
+    } else {
+      return cursorReplay({
+        playback,
+        target: ref.current,
+        ...props,
+        data: props.data
+      });
+    }
   }, [props.align, ref.current]);
 
   return (
