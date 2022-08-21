b0cf67e469c778462e4f5b5e9c4641548a7dd4f2
Yuri Sulyma <yuri@liqvidjs.org>
2022-08-21 01:31:37 -0400

    gsap: upgrade to MediaElement

---
+++ packages/gsap/src/EventEmitter.ts
@@ -0,0 +1,38 @@
+import type {MediaElementEventMap} from "@lqv/playback";
+
+export class EventEmitter {
+  private __listeners: Partial<Record<keyof MediaElementEventMap, (() => unknown)[]>>;
+
+  constructor() {
+    this.__listeners = {};
+  }
+
+  addEventListener<K extends keyof MediaElementEventMap>(type: K, listener: () => unknown): void {
+    if (!this.__listeners.hasOwnProperty(type)) {
+      this.__listeners[type] = [];
+    }
+    this.__listeners[type].push(listener);
+  }
+
+  removeEventListener<K extends keyof MediaElementEventMap>(
+    type: K,
+    listener: () => unknown
+  ): void {
+    if (!this.__listeners[type]) {
+      return;
+    }
+    const index = this.__listeners[type].indexOf(listener);
+    if (index !== -1) {
+      this.__listeners[type].splice(index, 1);
+    }
+  }
+
+  protected emit<K extends keyof MediaElementEventMap>(type: K): void {
+    if (!this.__listeners[type]) {
+      return;
+    }
+    for (const listener of this.__listeners[type]) {
+      listener();
+    }
+  }
+}
--- packages/gsap/src/index.tsx
+++ packages/gsap/src/index.tsx
@@ -1,32 +1,31 @@
-import type {Playback} from "@lqv/playback";
+import type {MediaElement} from "@lqv/playback";
 import {PlaybackContext} from "@lqv/playback/react";
-import {EventEmitter} from "events";
 import type {gsap} from "gsap";
-import React, {useRef} from "react";
+import {useRef} from "react";
+import {EventEmitter} from "./EventEmitter";
 
 // GSAP already provides this, but as a global type,
 // which causes TS to complain about private names
 type GSAPTimeline = ReturnType<typeof gsap.timeline>;
 
 // /** Wrapper component for using Liqvid plugins in GSAP. */
-export const Bridge: React.FC<React.PropsWithChildren<{
-  timeline: GSAPTimeline;
-}>> = (props) => {
-  const playback = useRef<Playback>();
-  if (!playback.current) {
-    playback.current = new GSAPPlayback(props.timeline) as unknown as Playback;
+export const Bridge: React.FC<
+  React.PropsWithChildren<{
+    timeline: GSAPTimeline;
+  }>
+> = (props) => {
+  const playback = useRef<GSAPPlayback>();
+  if (!playback.current || props.timeline !== playback.current.tl) {
+    playback.current = new GSAPPlayback(props.timeline);
   }
 
   return (
-    <PlaybackContext.Provider value={playback.current}>
-      {props.children}
-    </PlaybackContext.Provider>
+    <PlaybackContext.Provider value={playback.current}>{props.children}</PlaybackContext.Provider>
   );
 };
 
-class GSAPPlayback extends EventEmitter {
-  currentTime = 0;
-  paused = false;
+/** Fit GSAP to {@link MediaElement} interface. */
+class GSAPPlayback extends EventEmitter implements MediaElement {
   muted = false;
   playbackRate = 1;
   seeking = false;
@@ -41,32 +40,42 @@ class GSAPPlayback extends EventEmitter {
     // sync with GSAP
     const onUpdate = this.tl.eventCallback("onUpdate");
     this.tl.eventCallback("onUpdate", () => {
-      const time = this.tl.time() * 1000;
-      this.currentTime = time;
-      this.emit("timeupdate", time);
+      this.emit("timeupdate");
       if (onUpdate) {
         onUpdate();
       }
     });
   }
 
-  get duration() {
-    return this.tl.duration() * 1000;
+  get currentTime(): number {
+    return this.tl.time();
+  }
+
+  set currentTime(value: number) {
+    this.tl.time(value);
+  }
+
+  get duration(): number {
+    return this.tl.duration();
+  }
+
+  set duration(value: number) {
+    throw new Error("Setting duration not implemented");
+  }
+
+  get paused(): boolean {
+    return this.tl.paused();
+  }
+
+  set paused(value: boolean) {
+    this.tl.paused(value);
   }
 
   pause(): void {
     this.tl.pause();
   }
 
-  play(): void {
+  async play(): Promise<void> {
     this.tl.play();
-  }
-
-  seek(t: number): void {
-    this.tl.seek(t / 1000);
-  }
-
-  stop(): void {
-    throw new Error("Method not implemented.");
   }
 }
